2478156ea1947cbe139dfaee412e522bd7508961
Elaine Vuong <elaine.yt.vuong@gmail.com>
2022-06-21 12:33:20 -0400

    Fixed Board and Card Rerendering in useEffect
    Co-authored-by: Trent Cooper <00trent@protonmail.com>

--- client/src/components/board/Board.jsx
+++ client/src/components/board/Board.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom'; // hook that gives us access to the URL Parameters
 import { fetchBoard } from '../../features/boards/boards';
@@ -9,16 +9,18 @@ import BoardHeader from './BoardHeader'
 const Board = () => {
   const dispatch = useDispatch();
 
-  const path = useParams()[0]; //returns path: boards | cards
+  const path = useParams()[0];
   let { id } = useParams()
 
-  const [boardId, setBoardId] = useState(id)
+  const cards = useSelector((state) => state.cards)
+  const card = cards.find(c => c._id === id)
+
+  if (card) {
+    id = card.boardId
+  }
 
   const boards = useSelector((state) => state.boards)
   const board = boards.find(b => b._id === id)  
-  
-  const cards = useSelector((state) => state.cards)
-  const card = cards.find(c => c._id === id)
 
   const generateBoardHeader = (board) 
     ? (<BoardHeader key={board._id} title={board.title} />)
@@ -29,45 +31,15 @@ const Board = () => {
       dispatch(fetchBoard(id));
     } else if (path === 'cards') {
       dispatch(fetchCard({ id }));
-      setBoardId(card.boardId)
-      // set id to card.boardId
     }
   }, [dispatch, id, path]);
    
   return (
     <>
       {generateBoardHeader}
-      <ExistingList id={boardId}/>
+      <ExistingList id={id}/>
     </>
   )  
 }
 
 export default Board;
-
-  // const card = useSelector((state) => state.cards)[0]
-  // console.log(card)
- 
-  // if (path === 'cards' && card !== undefined) {
-  //   id = card.boardId
-  // }
-
-  /*
-   Path => cards
-    even within Board the URL is for the CardId
-    we need to get the BoardId
-
-    Redux Store (on refresh) is empty
-
-    If we render the Card Component First => Redux.card (MIGHT) have the single card
-
-    if it doesn't, since we have the url
-
-    from within Board => make a fetchCard request (from within Board)
-    and from that, you can update the Store => card.state
-
-    from the card.state, you can get the BoardId
-
-    and then you use fetchBoard => and useEffect ot update the Board State
-
-    and then render the Board Component
-  */
--- client/src/components/card/CardView.jsx
+++ client/src/components/card/CardView.jsx
@@ -1,18 +1,11 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from "react-redux"
+import React from 'react'
+import { useSelector } from "react-redux"
 import { useParams } from "react-router-dom"
-import { fetchCard } from '../../features/boards/cards'
 
 const CardView = () => {
-  const dispatch = useDispatch()
   const { cardId } = useParams()
   const card = useSelector((state) => state.cards).find(card => card._id === cardId) 
   const list = useSelector((state) => state.lists).find(list => card.listId === list._id)
-
-  useEffect(() => {
-    dispatch(fetchCard({ id: cardId }))
-  }, [dispatch, cardId])
-  
 
   const generateCardView = ( card && list )
     ? (<Card card={card} list={list} />)
